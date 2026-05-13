c04673938038108dd1cd5da7ffa4532d0eb16767
KiraihDuoc <ben.figueroan@duocuc.cl>
2026-05-13 10:00:57 -0400

    Delete rol directory

--- rol/src/main/java/cl/bibliotecaproyecto/rol/repository/RolRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.bibliotecaproyecto.rol.repository;
-
-import cl.bibliotecaproyecto.rol.model.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolRepository extends JpaRepository<Rol, Long> {
-}
